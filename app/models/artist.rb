73ef8976cf6fd1d490cbfcee931534c942e97a38
owenabbott <ocabbott@gmail.com>
2020-03-21 17:20:32 -0500

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -3,14 +3,22 @@ class Artist < ActiveRecord::Base
   has_many :genres, through: :songs
 
   def get_genre_of_first_song
+    first_song = Artist.first.songs[0].genre
     #return the genre of the artist's first saved song
   end
 
   def song_count
+    self.songs.count
     #return the number of songs associated with the artist
   end
 
   def genre_count
+    array = []
+    self.songs.each do |song|
+      array << song.genre 
+    end
+    array = array.uniq
+    array.count
     #return the number of genres associated with the artist
   end
 end
--- app/models/genre.rb
+++ app/models/genre.rb
@@ -3,14 +3,36 @@ class Genre < ActiveRecord::Base
   has_many :artists, through: :songs
 
   def song_count
-    # return the number of songs in a genre
+    songs = Song.where("genre_id = ?", self.id)
+    songs.count
+
   end
+    
+    # return the number of songs in a genre
 
   def artist_count
+    songs = Song.where("genre_id = ?", self.id)
+    artists = []
+    songs.each do |song|
+      artists << song.artist
+    end
+    artists = artists.uniq
+    artists.count
     # return the number of artists associated with the genre
   end
 
   def all_artist_names
+    all_of_them = []
+    songs = Song.where("genre_id = ?", self.id)
+    artists = []
+    songs.each do |song|
+      artists << song.artist
+    end
+    artists = artists.uniq
+    artists.each do |artist|
+      all_of_them << artist.name
+    end
+    all_of_them
     # return an array of strings containing every musician's name
   end
 end
